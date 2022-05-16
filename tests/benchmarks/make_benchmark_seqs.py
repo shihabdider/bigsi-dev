191b97f9f128f2e6c19f9a8dea705e4fb4c58bb3
Shihab Dider <shihabdider@gmail.com>
2022-05-15 20:09:17 -0700

    feat: add file based benchmark seq generation via pysam

--- tests/benchmarks/make_benchmark_seqs.py
+++ tests/benchmarks/make_benchmark_seqs.py
@@ -11,6 +11,24 @@ import pandas as pd
 import argparse
 import logging
 
+
+def get_pysam_record(fasta_path, identifier, start, end):
+    ref = pysam.FastaFile(fasta_path)
+    seq = ref.fetch(identifier, start, end)
+    record_id = '{0}:{1}-{2}'.format(identifier, start, end)
+    fasta_record = SeqRecord(Seq(seq.upper()), record_id)
+    return fasta_record
+
+
+
+def get_fasta_record(fasta_handle, identifier, start, end):
+    for record in SeqIO.parse(fasta_handle, 'fasta'):
+        if identifier == record.id:
+            sequence = str(record.seq[start:end])
+            fasta_record = SeqRecord(Seq(sequence), record.id)
+            return fasta_record
+
+
 def get_ncbi_record(identifier, start, end):
     '''Retrieves seq record from NCBI'''
     Entrez.email = 'shihabdider@berkeley.edu'
@@ -22,6 +40,23 @@ def get_ncbi_record(identifier, start, end):
     fasta_handle.close()
 
     return fasta_record
+
+
+def make_random_fasta_interval(faidx, identifier, interval_length):
+    ref_lengths = {}
+    with open(faidx, 'r') as handle:
+        for line in handle:
+            split_line = line.split('\t')
+            name = split_line[0]
+            length = int(split_line[1])
+            ref_lengths[name] = length
+
+    ref_length = ref_lengths[identifier]
+    # get the interval
+    interval_start = random.randint(0, ref_length - interval_length)
+    interval_end = interval_start + interval_length - 1
+
+    return interval_start, interval_end
 
 
 def make_random_interval(identifier, interval_length):
@@ -53,6 +88,11 @@ def main():
         description="Create sequence FASTA for running benchmark.s"
     )
     parser.add_argument(
+        "-f", "--fasta", type=str, 
+        help="path to fasta file", 
+        required=True
+    )
+    parser.add_argument(
         "-i", "--identifiers", type=str, 
         help="path to file containing NCBI identifiers (one per line)", 
         required=True
@@ -73,6 +113,7 @@ def main():
         required=True
     )
     args = parser.parse_args()
+    faidx = args.fasta + '.fai'
 
     identifiers = []
     with open(args.identifiers, 'r') as handle:
@@ -81,13 +122,15 @@ def main():
 
     print('Retrieving random records...')
     output_records = []
-    for identifier in identifiers:
-        for i in range(args.num):
-            rand_start, rand_end = make_random_interval(identifier,
-                                                        args.length)
-            random_record = get_ncbi_record(identifier, rand_start, rand_end)
-            print(identifier, i, random_record)
-            output_records.append(random_record)
+    with open(args.fasta, 'r') as handle:
+        for identifier in identifiers:
+            for i in range(args.num):
+                rand_start, rand_end = make_random_fasta_interval(
+                    faidx, identifier, args.length)
+                random_record = get_pysam_record(args.fasta, identifier,
+                                                 rand_start, rand_end)
+                print(identifier, i, random_record)
+                output_records.append(random_record)
 
     records_to_fasta(output_records, args.output)
 
