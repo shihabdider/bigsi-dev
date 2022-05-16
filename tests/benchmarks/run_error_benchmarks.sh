e600e041217f18a86655f86298046b9d6f5c690f
Shihab Dider <shihabdider@gmail.com>
2022-05-15 20:20:16 -0700

    feat: add scripts to run error and length benchmarks

---
+++ tests/benchmarks/run_error_benchmarks.sh
@@ -0,0 +1,7 @@
+python benchmark.py -q seqs/hg38_random_001.fasta -c hg38.config.json -o output/hg38_001 -i 99
+python benchmark.py -q seqs/hg38_random_002.fasta -c hg38.config.json -o output/hg38_002 -i 98
+python benchmark.py -q seqs/hg38_random_003.fasta -c hg38.config.json -o output/hg38_003 -i 97
+python benchmark.py -q seqs/hg38_random_004.fasta -c hg38.config.json -o output/hg38_004 -i 96
+python benchmark.py -q seqs/hg38_random_005.fasta -c hg38.config.json -o output/hg38_005 -i 95
+python benchmark.py -q seqs/hg38_random_006.fasta -c hg38.config.json -o output/hg38_006 -i 94
+python benchmark.py -q seqs/hg38_random_010.fasta -c hg38.config.json -o output/hg38_010 -i 90
---
+++ tests/benchmarks/run_length_benchmarks.sh
@@ -0,0 +1,4 @@
+python benchmark.py -q seqs/hg38_5KB.fasta -c hg38.config.json -o output/hg38_5KB -i 100
+python benchmark.py -q seqs/hg38_100KB.fasta -c hg38.config.json -o output/hg38_100KB -i 100
+python benchmark.py -q seqs/hg38_300KB.fasta -c hg38.config.json -o output/hg38_300KB -i 100
+
