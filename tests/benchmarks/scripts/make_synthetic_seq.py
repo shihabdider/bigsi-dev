0c2d3ca3a42b0bf24611bc0c5cbf489aa56c0d1d
Shihab Dider <shihabdider@gmail.com>
2022-05-21 20:42:13 -0700

    feat: add scripts to generate performance benchmark

--- tests/benchmarks/scripts/make_synthetic_seq.py
+++ tests/benchmarks/scripts/make_synthetic_seq.py
@@ -1,19 +1,44 @@
 import random
+import sys
+from Bio import SeqIO
+from Bio import Seq
+from Bio import SeqRecord
 
-dna = ["A", "G", "C", "T"]
-synthetic_seq = ''
-seq_length = 3*10e7
-print('Generating sequence of length: {0}'.format(seq_length))
 
-for i in range(1, int(seq_length+1)):
-    synthetic_seq += random.choice(dna)
-    if i % 80 == 0:
-        synthetic_seq += '\n'
+def make_synthetic_genome(seq_length):
+    dna = ["A", "G", "C", "T"]
+    genome_records = []
+    if seq_length > 100e6:
+        for i in range(0, seq_length, int(100e6)):
+            synthetic_id = 'synthetic_seq {}'.format(i)
+            synthetic_seq = make_synthetic_seq(dna, 100e6)
+            synth_record = SeqRecord(Seq(synthetic_seq), synthetic_id))
+            genome_records.append(synth_record)
+    else:
+            synthetic_id = 'synthetic_seq'
+            synthetic_seq = make_synthetic_seq(dna, seq_length)
+            synth_record = SeqRecord(Seq(synthetic_seq), synthetic_id))
+            genome_records.append(synth_record)
 
-#print('>synthetic_seq {0}'.format(seq_length))
-#print(synthetic_seq)
+    return genome_records
 
-fasta = 'seqs/synthetic_seq_300M.fasta'
-with open(fasta, 'w') as handle:
-    handle.write('>synthetic_seq {0}'.format(seq_length))
-    handle.write(synthetic_seq)
+
+def make_synthetic_seq(alphabet, seq_length):
+    synthetic_seq = ''
+    for i in range(1, int(seq_length+1)):
+        synthetic_seq += random.choice(alphabet)
+        if i % 80 == 0:
+            synthetic_seq += '\n'
+
+    return synthetic_seq
+
+
+def main():
+    seq_length = int(sys.argv[1])*10e6
+    fasta = sys.argv[2]
+    print('Generating sequence of length: {0}'.format(seq_length))
+    genome = make_synthetic_genome(seq_length)
+    with open(fasta, 'w') as handle:
+        SeqIO.write(genome, handle, 'fasta')
+
+main()
---
+++ tests/benchmarks/scripts/synth_memory_benchmark.sh
@@ -0,0 +1,10 @@
+BIGSI_SIZES=( 100 200 400 800 1600 2000 2500 3000)
+
+for size in ${BIGSI_SIZES[@]};
+do
+    time ../../../bin/query_bigsi.js \
+        -r ../../../bigsis/synth_$size.bigsi.bin \
+        -q seqs/synthetic/performance/synth_query_$size.fasta \
+        -c ../../../bigsi.query.synth_$size.config.json \
+        > metrics/synth_bigsi_size.txt
+done
