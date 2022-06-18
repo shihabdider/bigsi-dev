f1249200a1b3c6ec4065e60677b3db3806163ba0
shihabdider <shihabdider@gmail.com>
2022-06-18 11:28:21 -0700

    add metrics for 1 experiments

--- tests/benchmarks/scripts/generate_benchmarks.sh
+++ tests/benchmarks/scripts/generate_benchmarks.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-num_experiments=100;
+num_experiments=1;
 query_config="/Users/shihabdider/Research/bigsi-dev/bigsis/hg38_var_bins_test_query_config.json";
 mashmap_flag=0;
 script_dir="/Users/shihabdider/Research/bigsi-dev/tests/benchmarks/scripts";
@@ -12,19 +12,18 @@ echo "Running mashmap query: $mashmap_flag";
 # Load functions from scripts
 source "${script_dir}/run_simulation_benchmarks.sh";
 source "${script_dir}/get_metrics.sh";
-#source ./get_metrics.sh;
-#source ./run_simulation_benchmarks.sh;
 
 # Simulation benchmarks
-# error_benchmark && wait;
-# query_length_benchmark && wait;
-# mammal_benchmark "pan_trog" && wait;
-# mammal_benchmark "gorilla" && wait;
 
-sub_rate_metrics "sub_rate";
-query_length_metrics "query_length";
-mammal_metrics "pan_trog";
-mammal_metrics "gorilla";
+error_benchmark && wait;
+query_length_benchmark && wait;
+mammal_benchmark "pan_trog" && wait;
+mammal_benchmark "gorilla" && wait;
+
+# sub_rate_metrics "sub_rate";
+# query_length_metrics "query_length";
+# mammal_metrics "pan_trog";
+# mammal_metrics "gorilla";
 
 #nanopore_read_metrics sensitivity > metrics/nanopore_read_sensitivities_003.txt;
 #nanopore_read_metrics specificity > metrics/nanopore_read_specificities_003.txt; 
