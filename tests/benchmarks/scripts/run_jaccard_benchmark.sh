9099b035f649b83d0fea2af677179593ee813f18
shihabdider <shihabdider@gmail.com>
2022-06-19 21:17:49 -0700

    feat: add jaccard deviation benchmarks for 25 50 100 window sizes

--- tests/benchmarks/scripts/run_jaccard_benchmark.sh
+++ tests/benchmarks/scripts/run_jaccard_benchmark.sh
@@ -1,19 +1,12 @@
 
 function jaccard_benchmark() {
-    sub_rates=( 001 002 003 004 005 006 007 008 009 010 )
+    sub_rates=( 000 001 002 003 004 005 006 007 008 009 010 )
     ref_sizes=( 0.05 0.1 0.2 0.4 0.8 1.6 3 )
-    window_size=25
+    window_size=$1
     N=12
     # iterate through all refs
     for size in ${ref_sizes[@]};
     do
-        # handle exact match
-        ref="seqs/synthetic/jaccard/${size}.fasta"
-        query="seqs/synthetic/jaccard/${size}_query.fasta"
-        node ~/Research/bigsi-dev/tests/jaccard_test.js \
-            -r ${ref} -q ${query} -w ${window_size} > \
-            metrics/jaccard_test_${size}_000.txt
-
         # iterate through all mutated queries for that ref
         for rate in ${sub_rates[@]};
         do
@@ -22,11 +15,13 @@ function jaccard_benchmark() {
             query="seqs/synthetic/jaccard/${size}_query_${rate}.fasta"
             node ~/Research/bigsi-dev/tests/jaccard_test.js \
                 -r ${ref} -q ${query} -w ${window_size} > \
-                metrics/jaccard_test_${size}_${rate}.txt \
+                metrics/jaccard/${size}_${rate}_w${window_size}.txt \
         &
         done
         echo "Benchmarks for $size reference complete!"
     done
 }
 
-jaccard_benchmark
+jaccard_benchmark 25;
+jaccard_benchmark 50;
+jaccard_benchmark 100;
