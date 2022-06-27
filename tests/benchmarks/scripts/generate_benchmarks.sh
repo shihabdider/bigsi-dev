75f47c1fb6c5c0ca139898bb5c4f99513f53c28d
shihabdider <shihabdider@gmail.com>
2022-06-26 23:39:00 -0700

    add metrics for 2 experiments

--- tests/benchmarks/scripts/generate_benchmarks.sh
+++ tests/benchmarks/scripts/generate_benchmarks.sh
@@ -2,7 +2,7 @@
 
 num_experiments=2;
 query_config="/Users/shihabdider/Research/bigsi-dev/bigsis/hg38_var_bins_test_query_config.json";
-mashmap_flag=0;
+mashmap_flag=1;
 script_dir="/Users/shihabdider/Research/bigsi-dev/tests/benchmarks/scripts";
 commit_msg="add metrics for ${num_experiments} experiments";
 
@@ -20,10 +20,10 @@ query_length_benchmark && wait;
 sub_rate_metrics "sub_rate";
 query_length_metrics "query_length";
 
-#mammal_benchmark "pan_trog" && wait;
-#mammal_benchmark "gorilla" && wait;
-#mammal_metrics "pan_trog";
-#mammal_metrics "gorilla";
+mammal_benchmark "pan_trog" && wait;
+mammal_benchmark "gorilla" && wait;
+mammal_metrics "pan_trog";
+mammal_metrics "gorilla";
 
 #nanopore_read_metrics sensitivity > metrics/nanopore_read_sensitivities_003.txt;
 #nanopore_read_metrics specificity > metrics/nanopore_read_specificities_003.txt; 
