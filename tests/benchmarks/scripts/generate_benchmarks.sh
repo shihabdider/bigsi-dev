79f18d6cbbe35c5a1acd1f8818877446eb77eabe
shihabdider <shihabdider@gmail.com>
2022-06-27 07:50:14 -0700

    add metrics for 10 experiments

--- tests/benchmarks/scripts/generate_benchmarks.sh
+++ tests/benchmarks/scripts/generate_benchmarks.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-num_experiments=2;
-query_config="/Users/shihabdider/Research/bigsi-dev/bigsis/hg38_var_bins_test_query_config.json";
-mashmap_flag=1;
+num_experiments=10;
+query_config="/Users/shihabdider/Research/bigsi-dev/bigsis/hg38_w50_query_config.json";
+mashmap_flag=0;
 script_dir="/Users/shihabdider/Research/bigsi-dev/tests/benchmarks/scripts";
 commit_msg="add metrics for ${num_experiments} experiments";
 
