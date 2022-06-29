208ef81be63107ec6fb678e54b2c803ab930b887
shihabdider <shihabdider@gmail.com>
2022-06-29 04:43:52 -0700

    add metrics for 10 experiments

--- tests/benchmarks/scripts/generate_benchmarks.sh
+++ tests/benchmarks/scripts/generate_benchmarks.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-num_experiments=100;
+num_experiments=10;
 query_config="/Users/shihabdider/Research/bigsi-dev/bigsis/hg38_w50_query_config.json";
 mashmap_flag=0;
 script_dir="/Users/shihabdider/Research/bigsi-dev/tests/benchmarks/scripts";
@@ -18,20 +18,20 @@ source "${script_dir}/get_metrics.sh";
 
 error_benchmark && wait;
 query_length_benchmark && wait;
-sub_rate_metrics "sub_rate_w50_bound";
-query_length_metrics "query_length_w50_bound";
+sub_rate_metrics "sub_rate_w50_bound_05";
+query_length_metrics "query_length_w50_bound_05";
 
-mammal_benchmark "pan_trog" && wait;
-mammal_benchmark "gorilla" && wait;
-mammal_metrics "pan_trog";
-mammal_metrics "gorilla";
+#mammal_benchmark "pan_trog" && wait;
+#mammal_benchmark "gorilla" && wait;
+#mammal_metrics "pan_trog";
+#mammal_metrics "gorilla";
 
-pacbio_benchmark && wait;
-nanopore_benchmark;
-nanopore_read_metrics sensitivity > metrics/nanopore_read_sensitivities_w50.txt;
-nanopore_read_metrics specificity > metrics/nanopore_read_specificities_w50.txt; 
-pacbio_read_metrics sensitivity > metrics/pacbio_read_sensitivities_w50.txt;
-pacbio_read_metrics specificity > metrics/pacbio_read_specificities_w50.txt
+#pacbio_benchmark && wait;
+#nanopore_benchmark;
+#nanopore_read_metrics sensitivity > metrics/nanopore_read_sensitivities_w50.txt;
+#nanopore_read_metrics specificity > metrics/nanopore_read_specificities_w50.txt; 
+#pacbio_read_metrics sensitivity > metrics/pacbio_read_sensitivities_w50.txt;
+#pacbio_read_metrics specificity > metrics/pacbio_read_specificities_w50.txt
 
 # Push to remote repo
 git add . && git commit -m "${commit_msg}";
