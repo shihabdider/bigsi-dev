fafefb7c5305a74b8c5e3ccb0f37ff3afe87ad3a
shihabdider <shihabdider@gmail.com>
2022-07-06 14:58:17 -0700

    add metrics for 100 experiments

--- tests/benchmarks/scripts/generate_benchmarks.sh
+++ tests/benchmarks/scripts/generate_benchmarks.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-num_experiments=10;
+num_experiments=100;
 query_config="/Users/shihabdider/Research/bigsi-dev/bigsis/hg38_var_bins_test_query_config.json";
 mashmap_flag=0;
 script_dir="/Users/shihabdider/Research/bigsi-dev/tests/benchmarks/scripts";
@@ -21,10 +21,10 @@ query_length_benchmark && wait;
 sub_rate_metrics "sub_rate_w100_adaptive_bias";
 query_length_metrics "query_length_w100_adaptive_bias";
 
-#mammal_benchmark "pan_trog" && wait;
-#mammal_benchmark "gorilla" && wait;
-#mammal_metrics "pan_trog";
-#mammal_metrics "gorilla";
+mammal_benchmark "pan_trog" && wait;
+mammal_benchmark "gorilla" && wait;
+mammal_metrics "pan_trog";
+mammal_metrics "gorilla";
 
 #pacbio_benchmark && wait;
 #nanopore_benchmark;
