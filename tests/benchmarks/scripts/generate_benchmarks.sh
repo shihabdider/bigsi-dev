4abad7cdb951e3b4d751f76ea44d00d85496ddd0
shihabdider <shihabdider@gmail.com>
2022-07-23 04:04:19 -0700

    add metrics for 10 experiments

--- tests/benchmarks/scripts/generate_benchmarks.sh
+++ tests/benchmarks/scripts/generate_benchmarks.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 num_experiments=10;
-query_config="/Users/shihabdider/Research/bigsi-dev/bigsis/hg38_many_bins_query_config.json";
+query_config="/Users/shihabdider/Research/bigsi-dev/bigsis/hg38_32M_bins_query_config.json";
 mashmap_flag=1;
 script_dir="/Users/shihabdider/Research/bigsi-dev/tests/benchmarks/scripts";
 commit_msg="add metrics for ${num_experiments} experiments";
@@ -18,8 +18,8 @@ source "${script_dir}/get_metrics.sh";
 
 error_benchmark && wait;
 query_length_benchmark && wait;
-sub_rate_metrics "sub_rate_many_bins_adaptive_99999995";
-query_length_metrics "query_length_many_bins_adaptive_99999995";
+sub_rate_metrics "sub_rate_32M_bins_filter_adaptive_999995";
+query_length_metrics "query_length_32M_bins_filter_999995";
 
 #mammal_benchmark "pan_trog" && wait;
 #mammal_benchmark "gorilla" && wait;
