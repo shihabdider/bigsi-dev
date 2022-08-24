c79116b462ba005704204622435e9c43f06c9f11
shihabdider <shihabdider@gmail.com>
2022-08-24 14:22:53 -0700

    add metrics for 100 experiments

--- tests/benchmarks/test.sh
+++ tests/benchmarks/test.sh
@@ -1,2 +1,17 @@
-HG38_SIM=hg38/simulation
-echo metrics/${HG38_SIM}/error_rate_trials_sensitivity.txt \
+#HG38_SIM=hg38/simulation
+#echo metrics/${HG38_SIM}/error_rate_trials_sensitivity.txt \
+
+errs=( 001 002 003 004 005 )
+lengths=( 1000 2000 3000 4000 5000 10000 20000 40000 80000 160000 200000 250000 300000 )
+params=( )
+for err in ${errs[@]}; do
+    for length in ${lengths[@]}; do
+        param="${length}_${err}"
+        params+=( "$param" )
+    done
+done
+
+for value in "${params[@]}"
+do
+     echo $value
+done
