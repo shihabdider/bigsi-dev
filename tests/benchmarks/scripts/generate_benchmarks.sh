86f4fae0c22a8ffa25f6d8c94d03c9d148f5b3bb
shihabdider <shihabdider@gmail.com>
2022-09-22 16:33:48 -0700

    add metrics for 10 experiments

--- tests/benchmarks/scripts/generate_benchmarks.sh
+++ tests/benchmarks/scripts/generate_benchmarks.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Global parameters
-QUERY_LENGTHS=( 1000 2000 3000 4000 5000 7500 10000 ) # 12500 15000 17500 20000 ) # 40000 80000 160000 200000 250000 300000 )
-#QUERY_LENGTHS=( 7500 ) # 12500 15000 17500 20000 ) # 40000 80000 160000 200000 250000 300000 )
+#QUERY_LENGTHS=( 1000 2000 3000 4000 5000 7500 10000 ) # 12500 15000 17500 20000 ) # 40000 80000 160000 200000 250000 300000 )
+QUERY_LENGTHS=( 7500 ) # 12500 15000 17500 20000 ) # 40000 80000 160000 200000 250000 300000 )
 SUB_RATES=( 001 002 003 004 005 006 007 008 009 010 )
 
-num_experiments=50;
+num_experiments=10;
 query_config="/Users/shihabdider/Research/bigsi-dev/bigsis/hg38_32M_bins_query_config.json";
 mashmap_flag=1;
 bin_map="/Users/shihabdider/Research/bigsi-dev/bigsis/hg38_32M_bins_bucket_map.json"
@@ -29,10 +29,10 @@ source "${script_dir}/get_metrics.sh";
 #sub_rate_metrics "sub_rate_95_32M";
 #query_length_metrics "query_length_95_32M";
 
-#mammal_benchmark "pan_trog" && wait;
-#mammal_benchmark "gorilla" && wait;
-mammal_metrics "pan_trog";
-mammal_metrics "gorilla";
+mammal_benchmark "pan_trog" && wait;
+mammal_benchmark "gorilla" && wait;
+#mammal_metrics "pan_trog";
+#mammal_metrics "gorilla";
 
 #pacbio_benchmark && wait;
 #nanopore_benchmark;
