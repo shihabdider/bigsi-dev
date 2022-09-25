703d7010046a6eb8c906fb9530ceb00af20a1fb6
shihabdider <shihabdider@gmail.com>
2022-09-24 19:23:49 -0700

    add metrics for 50 experiments

--- tests/benchmarks/scripts/generate_benchmarks.sh
+++ tests/benchmarks/scripts/generate_benchmarks.sh
@@ -6,7 +6,7 @@ QUERY_LENGTHS=( 1000 2000 3000 4000 5000 7500 10000 12500 15000 17500 20000 ) # 
 #SUB_RATES=( 001 002 003 004 005 006 007 008 009 010 )
 SUB_RATES=( 006 )
 
-num_experiments=10;
+num_experiments=50;
 query_config="/Users/shihabdider/Research/bigsi-dev/bigsis/hg38_32M_bins_query_config.json";
 mashmap_flag=1;
 bin_map="/Users/shihabdider/Research/bigsi-dev/bigsis/hg38_32M_bins_bucket_map.json"
@@ -23,7 +23,7 @@ source "${script_dir}/get_metrics.sh";
 
 # Simulation benchmarks
 
-#error_and_length_benchmark && wait;
+error_and_length_benchmark && wait;
 error_and_length_metrics "query_length_006";
 #error_benchmark && wait;
 #query_length_benchmark && wait;
