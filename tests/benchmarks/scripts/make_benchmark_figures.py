04cda4f3caecbd3b3ba81f9db12d3627797454ee
Shihab Dider <shihabdider@gmail.com>
2022-10-01 14:49:07 -0700

    chore: prep for clean up

--- tests/benchmarks/scripts/make_benchmark_figures.py
+++ tests/benchmarks/scripts/make_benchmark_figures.py
@@ -2,12 +2,17 @@ import numpy as np
 import math
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.ticker
+from matplotlib.transforms import Bbox, TransformedBbox, blended_transform_factory
+from mpl_toolkits.axes_grid1.inset_locator import BboxPatch, BboxConnector,\
+    BboxConnectorPatch
 plt.style.use('seaborn-whitegrid')
 
 # Globals
 ERROR_RATES = [0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.1]
+# SEQ_LENGTHS = [1000, 2000, 3000, 4000, 5000, 7500, 10000]
 SEQ_LENGTHS = [1000, 2000, 3000, 4000, 5000, 7500, 10000, 12500, 15000, 17500,
-               20000, 40000, 80000]
+               20000]
 
 
 def make_simulation_df(file, parameter_name, metric_name):
@@ -90,18 +95,21 @@ def make_experiments_figure(xs, ys, metrics):
     returns: figure and axis objects
     '''
 
-    fig, axs = plt.subplots(1, 2, sharey='row', figsize=(8, 6))
+    fig, axs = plt.subplots(1, len(ys), sharey='row', figsize=(12, 6))
 
     for i, ax in enumerate(axs):
         for metric in metrics:
+            xs = ys[i]['{0}_means'.format(metric['name'])].index
             ax.plot(
-                xs[i] if i < len(xs) else xs[0],
+                xs,
+                # xs[i] if i < len(xs) else xs[0],
                 ys[i]['{0}_means'.format(metric['name'])],
                 label=metric['name'] if i == 0 else "",
                 marker='o'
             )
             ax.errorbar(
-                xs[i] if i < len(xs) else xs[0],
+                xs,
+                # xs[i] if i < len(xs) else xs[0],
                 ys[i]['{0}_means'.format(metric['name'])],
                 yerr=[
                     ys[i]['{0}_errors'.format(metric['name'])].lower,
@@ -116,8 +124,13 @@ def make_experiments_figure(xs, ys, metrics):
 def make_simulation_trials_figure(figure_output=False):
     sub_stats = get_simulation_stats('sub_rate_95_32M', 'sub_rate')
     length_stats = get_simulation_stats('query_length_95_32M', 'query_length')
-    xs = [ERROR_RATES, SEQ_LENGTHS]
-    ys = [sub_stats, length_stats]
+
+    sub_stats_5kb = get_simulation_stats('sub_rate_5000', 'sub_rate')
+    length_stats_006 = get_simulation_stats('query_length_006', 'query_length')
+
+    xs = [ERROR_RATES, SEQ_LENGTHS, ERROR_RATES, SEQ_LENGTHS]
+    ys = [sub_stats, length_stats, sub_stats_5kb, length_stats_006]
+
     metrics = [
         {'name': 'sensitivity', 'color': 'blue'},
         {'name': 'specificity', 'color': 'orange'}
@@ -141,7 +154,6 @@ def make_simulation_trials_figure(figure_output=False):
                                        1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0,
                                        1.0, 1.0, 1.0]
 
-
     # Theoritical Curves
     axs[0].plot(
         ERROR_RATES,
@@ -163,20 +175,35 @@ def make_simulation_trials_figure(figure_output=False):
         query_size_theory_specificities[:11],
     )
 
-    # Substitution Rate
-    # ax1.axhline(y=0.95, linestyle='--', color='grey')
     fig.suptitle('Flashmap accuracy on simulated human genome sequences')
-    axs[0].axvline(x=0.05, linestyle='--', color='grey')
-    axs[0].text(0.045, 0.1, '0.05 substitution rate threshold', rotation=90)
+
+    # Substitution Rate
+    axs[0].set_title('10Kb query')
+    axs[0].axvline(x=0.06, linestyle='--', color='grey')
+    axs[0].text(0.045, 0.1, '0.06 substitution rate threshold', rotation=90)
     axs[0].set_xlabel('Substitutions per site')
-    # axs[1].set_ylim(ymin=0.7)
+
+    axs[2].set_title('5Kb query')
+    axs[2].axvline(x=0.06, linestyle='--', color='grey')
+    axs[2].text(0.045, 0.1, '0.06 substitution rate threshold', rotation=90)
+    axs[2].set_xlabel('Substitutions per site')
 
     # Query Length
+    axs[1].set_title('Exact matching query')
     axs[1].axvline(x=5000, linestyle='--', color='grey')
     axs[1].text(5500, 0.3, '5kb query threshold', rotation=90)
-    # axs[2].set_xscale('log')
+    # axs[1].set_xscale('log')
     axs[1].set_xlabel('Query length (kb)')
-    fig.legend(frameon=True)
+    axs[1].set_xticks([1000, 5000, 10000, 15000, 20000])
+
+    axs[3].set_title('0.06 substitution rate query')
+    axs[3].axvline(x=5000, linestyle='--', color='grey')
+    axs[3].text(5500, 0.3, '5kb query threshold', rotation=90)
+    # axs[3].set_xscale('log')
+    axs[3].set_xlabel('Query length (kb)')
+    axs[3].set_xticks([1000, 5000, 10000, 15000, 20000])
+
+    fig.legend(loc='center right', frameon=False)
 
     if figure_output:
         plt.savefig(figure_output)
@@ -194,11 +221,13 @@ def get_read_metrics(file):
     return metrics
 
 def make_mammal_figure(figure_output=False):
-    pan_trog = get_simulation_stats('pan_trog', 'query_length')
-    gorilla = get_simulation_stats('gorilla', 'query_length')
+    ys = []
+    species = ['pan_trog', 'gorilla', 'bonobo']
+    for specie in species:
+        y = get_simulation_stats(specie, 'query_length')
+        ys.append(y)
 
     xs = [SEQ_LENGTHS]
-    ys = [pan_trog, gorilla]
     metrics = [
         {'name': 'sensitivity', 'color': 'blue'},
         {'name': 'specificity', 'color': 'orange'}
@@ -210,33 +239,26 @@ def make_mammal_figure(figure_output=False):
          'mapped to human reference hg38')
     )
 
-    # Pan Trog
-    axs[0].set_title('Chimpanzee')
-    axs[0].axvline(x=5000, linestyle='--', color='grey')
-    axs[0].text(5500, 0.2, '5kb query threshold', rotation=90)
-    axs[0].set_xscale('log')
-    axs[0].set_xlabel('Query length (kb)')
+    for ax in axs:
+        ax.axvline(x=5000, linestyle='--', color='grey')
+        ax.text(5500, 0.3, '5kb query threshold', rotation=90)
+        ax.set_xscale('log')
+        ax.set_xlabel('Query length (kb)')
+        ax.set_xticks([1000, 2500, 5000, 10000, 20000])
+        ax.get_xaxis().set_major_formatter(matplotlib.ticker.ScalarFormatter())
 
-    # Gorilla
-    # axs[1].set_ylim(ymin=0)
+    # Set titles
+    axs[0].set_title('Chimpanzee')
     axs[1].set_title('Gorilla')
-    axs[1].text(5500, 0.2, '5kb query threshold', rotation=90)
-    axs[1].axvline(x=5000, linestyle='--', color='grey')
-    axs[1].set_xscale('log')
-    axs[1].set_xlabel('Query length (kb)')
+    axs[2].set_title('Bonobo')
+    # axs[3].set_title('Basset')
+    fig.legend()
 
     if figure_output:
         plt.savefig(figure_output)
     else:
         plt.show()
 
-
-def test():
-    # file = './metrics/sub_rate_995_32M_sensitivity.txt'
-    # make_simulation_trials_figure()
-    make_mammal_figure()
-
-# test()
 
 
 def make_synth_figure():
@@ -265,8 +287,8 @@ def make_synth_figure():
     #ax1.errorbar(error_rates, subs_specificity_means,
     #             yerr=[subs_specificity_errors_lower,
     #                   subs_specificity_errors_upper], fmt='-', color='orange')
-    ax1.axvline(x=0.05, linestyle='--', color='grey')
-    ax1.text(0.045, 0.3, '0.05 substitution rate threshold', rotation=90)
+    ax1.axvline(x=0.06, linestyle='--', color='grey')
+    ax1.text(0.045, 0.3, '0.06 substitution rate threshold', rotation=90)
     ax1.set_xlabel('Substitutions per site')
     fig.legend()
 
@@ -468,6 +490,112 @@ def make_window_size_boundary_points(genome_name):
     boundary_points = pd.DataFrame(boundary_points).sort_values(by='x')
     return boundary_points
 
+
+def connect_bbox(bbox1, bbox2,
+                 loc1a, loc2a, loc1b, loc2b,
+                 prop_lines, prop_patches=None):
+    if prop_patches is None:
+        prop_patches = prop_lines.copy()
+        prop_patches["alpha"] = prop_patches.get("alpha", 1)*0.2
+
+    c1 = BboxConnector(bbox1, bbox2, loc1=loc1a, loc2=loc2a, **prop_lines)
+    c1.set_clip_on(False)
+    c2 = BboxConnector(bbox1, bbox2, loc1=loc1b, loc2=loc2b, **prop_lines)
+    c2.set_clip_on(False)
+
+    bbox_patch1 = BboxPatch(bbox1, **prop_patches)
+    bbox_patch2 = BboxPatch(bbox2, **prop_patches)
+
+    p = BboxConnectorPatch(bbox1, bbox2,
+                           loc1a=loc1a, loc2a=loc2a, loc1b=loc1b, loc2b=loc2b,
+                           **prop_patches)
+    p.set_clip_on(False)
+
+    return c1, c2, bbox_patch1, bbox_patch2, p
+
+
+
+
+def zoom_effect02(ax1, ax2, **kwargs):
+    """
+    ax2 : the big main axes
+    ax1 : the zoomed axes
+    The xmin & xmax will be taken from the
+    ax1.viewLim.
+    """
+
+    tt = ax1.transScale + (ax1.transLimits + ax2.transAxes)
+    trans = blended_transform_factory(ax2.transData, tt)
+
+    mybbox1 = ax1.bbox
+    mybbox2 = TransformedBbox(ax1.viewLim, trans)
+
+    prop_patches = kwargs.copy()
+    prop_patches["ec"] = "none"
+    prop_patches["alpha"] = 0.2
+
+    c1, c2, bbox_patch1, bbox_patch2, p = \
+        connect_bbox(mybbox1, mybbox2,
+                     loc1a=2, loc2a=3, loc1b=1, loc2b=4, 
+                     prop_lines=kwargs, prop_patches=prop_patches)
+
+    ax1.add_patch(bbox_patch1)
+    ax2.add_patch(bbox_patch2)
+    ax2.add_patch(c1)
+    ax2.add_patch(c2)
+    ax2.add_patch(p)
+
+    return c1, c2, bbox_patch1, bbox_patch2, p
+
+
+def test():
+    # file = './metrics/sub_rate_995_32M_sensitivity.txt'
+    # make_simulation_trials_figure()
+    t = np.arange(1000)
+    x1 = np.sin(np.arange(1000)/10.)
+    x2 = np.cos(np.arange(1000)/10.)
+
+    plt.figure()
+    ax1 = plt.subplot(2, 2, 1)
+    plt.plot(t, x1)
+    ax2 = plt.subplot(2, 2, 2)
+    plt.plot(t, x2)
+
+    ax3 = plt.subplot(2, 2, 3)
+    plt.plot(t[100:200], x1[100:200])
+    zoom_effect02(ax3, ax1)
+
+    ax4 = plt.subplot(2, 2, 4)
+    plt.plot(t[600:700], x2[600:700])
+    zoom_effect02(ax4, ax2)
+    plt.show()
+
+
+# test()
+
+def make_window_size_zoomed(main_axis, position, data, x_lim):
+    ax = plt.subplot(position)
+    plt.plot(
+        data.x[:x_lim], 
+        data.y[:x_lim]
+    )
+    ax.fill_between(
+        data.x[:x_lim], 
+        94, data.y[:x_lim], 
+        where=(
+            (data.y[:x_lim] >= 94) & 
+            (data.x[:x_lim] >= 5000)
+        ),
+    )
+    ax.fill_between(
+        data.x[:x_lim], 
+        100, data.y[:x_lim],
+    )
+    ax.axhline(y=94, linestyle='--', color='grey')
+    ax.axvline(x=5000, linestyle='--', color='grey')
+    zoom_effect02(ax, main_axis)
+
+
 def window_size_figure(figure_output=False):
     genomes = ['worm', 'hg38', 'plant']
     window_size_boundary_points = {
@@ -476,70 +604,66 @@ def window_size_figure(figure_output=False):
         boundary_points = make_window_size_boundary_points(genome)
         window_size_boundary_points[genome] = boundary_points
 
-    fig, axs = plt.subplots(
-        1, 
-        len(genomes), 
-        figsize=(12, 6),
-        sharey=True
-    )
-
-    for i, ax in enumerate(axs):
-        genome = genomes[i]
-        axs[i].set_title(genome)
-        axs[i].plot(
-            window_size_boundary_points[genome].x, 
-            window_size_boundary_points[genome].y
+    plt.figure(figsize=(10, 6))
+    axs = []
+    for i, genome in enumerate(genomes):
+        ax = plt.subplot(2, 3, i + 1)
+        plt.plot(
+                window_size_boundary_points[genome].x, 
+                window_size_boundary_points[genome].y
         )
-
-        axs[i].fill_between(
+        ax.fill_between(
             window_size_boundary_points[genome].x, 
             94, window_size_boundary_points[genome].y, 
             where=(
                 (window_size_boundary_points[genome].y >= 94) & 
                 (window_size_boundary_points[genome].x >= 5000)
             ),
-            label='Bigsi filter is required'
+            label='Bigsi filter is required' if i == 0 else ""
         )
-        axs[i].fill_between(
+        ax.fill_between(
             window_size_boundary_points[genome].x, 
             100, window_size_boundary_points[genome].y,
-            label='MashMap only',
+            label='MashMap only' if i == 0 else "",
         )
-        axs[i].axhline(y=94, linestyle='--', color='grey')
-        axs[i].axvline(x=5000, linestyle='--', color='grey')
+        ax.axhline(y=94, linestyle='--', color='grey')
+        ax.axvline(x=5000, linestyle='--', color='grey')
+        axs.append(ax)
 
-    fig.suptitle(
+    make_window_size_zoomed(
+        axs[1], 
+        223, 
+        window_size_boundary_points['hg38'], 
+        20
+    )
+
+    make_window_size_zoomed(
+        axs[2], 
+        224, 
+        window_size_boundary_points['plant'], 
+        100
+    )
+
+    plt.suptitle(
         'Decision boundary for using BIGSI filter for different genome sizes'
     )
-    # add a big axis, hide frame
-    plt.legend(loc='upper right', frameon=True)
-    fig.add_subplot(111, frameon=False)
-    # hide tick and tick label of the big axis
-    plt.tick_params(
-        labelcolor='none', 
-        which='both', 
-        top=False, 
-        bottom=False, 
-        left=False, 
-        right=False
-    )
-    plt.xlabel("Query Length")
-    plt.ylabel("Percent Identity")
-    # plt.xlabel('Query Length')
-    # plt.ylabel('Percent Identity')
-    # plt.xlim(1000, max(df.x))
+
+    plt.figtext(0.5, 0.04, 'Query Length', ha='center')
+    plt.figtext(0.04, 0.5, 'Percent Identity', va='center',
+                rotation='vertical')
+    plt.figlegend()
+
     if figure_output:
         plt.savefig(figure_output)
     else:
         plt.show()
 
 
-window_size_figure()  # figure_output='figures/window_size_figure')
+window_size_figure(figure_output='figures/window_size_figure')
 
 # make_read_figure()
 # make_runtime_figure()
-# make_simulation_trials_figure(10,
-#                               figure_output='figures/simulation_trials_95_32M')
-# make_mammal_figure(100)
+# make_simulation_trials_figure(figure_output='figures/simulation_trials_95_32M')
+# make_mammal_figure(figure_output='figures/mammal_accuracy')
 # make_synth_figure()
 # make_jaccard_test_figure(50)
--- tests/benchmarks/test_parallel.sh
+++
@@ -1,6 +0,0 @@
-N=4
-for j in {1..5};
-do
-    ((i=i%N)); ((i++==0)) && wait
-    sleep 6 &
-done
